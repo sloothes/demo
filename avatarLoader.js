8ad69c9fb2e8e1ef71b42db9e221fb9aa430caa9
sloothes <sloothes.com@gmail.com>
2019-08-17 23:02:43 +0300

    Update avatarLoader.js

--- avatarLoader.js
+++ avatarLoader.js
@@ -8,81 +8,16 @@
 
 (function(){
 
-    Promise.resolve().then(function(){
+    Promise.resolve( store("Avatar") ).then(function(json){
 
-    //  male.
-
-        var json = {};
-        return db.collection("male").find().forEach(
-            function(doc){
-                json[doc._id] = doc;
-            }, 
-            function(err){
-                if (err) throw err;
-            }
-        ).catch(function(err){
-            console.error(err);
-        }).then(function(){
-            return localPlayer.outfit.fromJSON(json);
-        }).then(function(outfit){
-            male = outfit;
-            debugMode && console.log({"male":male});
-            return;
-        });
-
-    }).then(function(){
-
-    //  female.
-
-        var json = {};
-        return db.collection("female").find().forEach(
-            function(doc){
-                json[doc._id] = doc;
-            }, 
-            function(err){
-                if (err) throw err;
-            }
-        ).catch(function(err){
-            console.error(err);
-        }).then(function(){
-            return localPlayer.outfit.fromJSON(json);
-        }).then(function(outfit){
-            female = outfit;
-            debugMode && console.log({"female":female});
-            return;
-        });
-
-    }).then(function(){
-
-    //  skeleton.
-
-        return db.collection("skeleton")
-        .findOne({_id:"body"}, function(err){
-            if (err) throw err;
-        }).then(function(doc){
-            return doc;
-        }).catch(function(err){
-            console.error(err);
-        }).then(function(doc){
-            return localPlayer.outfit.fromJSON({"skeleton":doc});
-        }).then(function(outfit){
-            skeleton = outfit.skeleton;
-            debugMode && console.log({"skeleton":skeleton});
-            return;
-        });
-
-    }).then(function(){
+        if (!json) throw "Null json data!";
 
     //  avatar.
-
-        return store("Avatar");
-
-    }).then(function(json){
-        if (!json) throw "Null avatar json data!";
 
         return localPlayer.outfit.fromJSON( json );
 
     }).then(function(outfit){
+
         if (!outfit) throw "Null outfit object!";
 
         if ( localPlayer.outfit.getGender("male") )
@@ -93,8 +28,77 @@
     }).catch(function(err){
         console.error(err);
 
-    //  Drop to female avatar.
-        localPlayerHandler("/gender/female");
+    //  Drop to skinned mesh loader.
+
+        return Promise.resolve().then(function(){
+
+        //  male.
+
+            var json = {};
+            return db.collection("male").find().forEach(
+                function(doc){
+                    json[doc._id] = doc;
+                }, 
+                function(err){
+                    if (err) throw err;
+                }
+            ).catch(function(err){
+                console.error(err);
+            }).then(function(){
+                return localPlayer.outfit.fromJSON(json);
+            }).then(function(outfit){
+                male = outfit;
+                debugMode && console.log({"male":male});
+                return;
+            });
+
+        }).then(function(){
+
+        //  female.
+
+            var json = {};
+            return db.collection("female").find().forEach(
+                function(doc){
+                    json[doc._id] = doc;
+                }, 
+                function(err){
+                    if (err) throw err;
+                }
+            ).catch(function(err){
+                console.error(err);
+            }).then(function(){
+                return localPlayer.outfit.fromJSON(json);
+            }).then(function(outfit){
+                female = outfit;
+                debugMode && console.log({"female":female});
+                return;
+            });
+
+        }).then(function(){
+
+        //  skeleton.
+
+            return db.collection("skeleton")
+            .findOne({_id:"body"}, function(err){
+                if (err) throw err;
+            }).then(function(doc){
+                return doc;
+            }).catch(function(err){
+                console.error(err);
+            }).then(function(doc){
+                return localPlayer.outfit.fromJSON({"skeleton":doc});
+            }).then(function(outfit){
+                skeleton = outfit.skeleton;
+                debugMode && console.log({"skeleton":skeleton});
+                return;
+            });
+
+        }).then(function(){
+
+        //  Drop to female avatar.
+            localPlayerHandler("/gender/female");
+            return;
+        });
 
     }).then(function(){
 
