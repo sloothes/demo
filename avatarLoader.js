a339556db99a00fe03df6df27abac9436f18f960
sloothes <sloothes.com@gmail.com>
2019-08-26 23:11:56 +0300

    Update avatarLoader.js

--- avatarLoader.js
+++ avatarLoader.js
@@ -1,10 +1,152 @@
-//  avatarLoader.js (v6.3)
+//  avatarLoader.js (v6.4)
 
     var male, female, skeleton;
 
     localPlayerHandler("/turn/back");
 //  Disable outfit direction visible on startup.
 //  localPlayer.outfit.direction.visible = false;
+
+(function(){
+
+//  IndexedDB first.
+
+    var dbUSER = new zango.Db( "USER", {
+        "current": true,
+    });
+
+    var selector = {_id:"avatar"};
+    var collection = dbUSER.collection("current");
+
+    collection.findOne(selector, function(err){
+        if (err) throw err;
+    }).then(function(json){
+        if (!json) throw "Current avatar not found!";
+        return localPlayer.outfit.fromJSON( json );
+    }).then(function(outfit){
+        if ( localPlayer.outfit.getGender("male") )
+            male = outfit;
+        else if ( localPlayer.outfit.getGender("female") )
+            female = outfit;
+    }).catch(function(err){
+        console.error(err);
+
+    //  Try localStorage.
+
+        var json = store("Avatar");
+        if (!json) throw "Avatar not found!";
+
+        localPlayer.outfit.fromJSON( json )
+        .then(function(outfit){
+            if ( localPlayer.outfit.getGender("male") )
+                male = outfit;
+            else if ( localPlayer.outfit.getGender("female") )
+                female = outfit;
+        }).then(function(){
+
+        //  Update database.
+            json._id = "avatar";
+            collection.insert(json, function(err){
+                if (err) throw err;
+            }).catch(function(err){
+                console.log(err);
+            });
+
+        }).catch(function(err){
+            console.error(err);
+
+        //  Drop to skinned mesh loader.
+
+            return Promise.resolve().then(function(){
+
+            //  male.
+
+                var json = {};
+                return db.collection("male").find().forEach(
+                    function(doc){
+                        json[doc._id] = doc;
+                    }, 
+                    function(err){
+                        if (err) throw err;
+                    }
+                ).catch(function(err){
+                    console.error(err);
+                }).then(function(){
+                    return localPlayer.outfit.fromJSON(json);
+                }).then(function(outfit){
+                    male = outfit;
+                    debugMode && console.log({"male":male});
+                    return;
+                });
+
+            }).then(function(){
+
+            //  female.
+
+                var json = {};
+                return db.collection("female").find().forEach(
+                    function(doc){
+                        json[doc._id] = doc;
+                    }, 
+                    function(err){
+                        if (err) throw err;
+                    }
+                ).catch(function(err){
+                    console.error(err);
+                }).then(function(){
+                    return localPlayer.outfit.fromJSON(json);
+                }).then(function(outfit){
+                    female = outfit;
+                    debugMode && console.log({"female":female});
+                    return;
+                });
+
+            }).then(function(){
+
+            //  skeleton.
+
+                return db.collection("skeleton")
+                .findOne({_id:"body"}, function(err){
+                    if (err) throw err;
+                }).then(function(doc){
+                    return doc;
+                }).catch(function(err){
+                    console.error(err);
+                }).then(function(doc){
+                    return localPlayer.outfit.fromJSON({"skeleton":doc});
+                }).then(function(outfit){
+                    skeleton = outfit.skeleton;
+                    debugMode && console.log({"skeleton":skeleton});
+                    return;
+                });
+
+            }).then(function(){
+
+            //  Drop to female avatar.
+                localPlayerHandler("/gender/female");
+                return;
+            });
+
+        });
+
+    }).then(function(){
+
+    //  Startup.
+
+    //  Enable outfit direction visible.
+        localPlayer.outfit.direction.visible = true;
+
+    //  Hide loading bar.
+        if (window.bootbox) window.bootbox.hideAll();
+
+    });
+
+})();
+
+
+
+
+/*
+//  avatarLoader.js (v6.3)
 
 (function(){
 
@@ -113,3 +255,4 @@
     });
 
 })();
+*/
